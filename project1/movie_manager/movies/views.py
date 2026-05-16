c7052082a839729c9ab1307cc1d04ed9a0e363ca
ADARSHKHVKD <adarshkhvkd@gmail.com>
2026-05-16 11:17:54 +0530

    first commit

--- project1/movie_manager/movies/views.py
+++ project1/movie_manager/movies/views.py
@@ -2,11 +2,48 @@ from django.shortcuts import render
 
 # Create your views here.
 def create(request):
+    if request.POST:
+        print(request.POST.get('title'))
+        print(request.POST.get('year'))
     return render(request,'create.html')
+movie_details={
+        'movies':[
+        {
+        'title':'DDLJ',
+        'year':1999,
+        'summary':'love story of two young people',
+        'success':True,
+        'img':'logo.webp'
+    },
+    {
+        'title':'godfather',
+        'year':1989,
+      
+        'success':True
+    },
+    {
+        'title':'forest gump',
+        'year':2000,
+        'summary':'motivational film',
+        'success':True
+    },
+    {
+        'title':'shawshank redemption',
+        'year':2001,
+        'summary':'life of prisoners',
+        'success':True
+    },
+    {
+        'title':'life of pi',
+        'year':2005,
+        'summary':'adventure of young boy',
+        'success':True
+    }
 
+    ]}
 
 def list(request):
-    return render(request,'list.html')
+    return render(request,'list.html',movie_details)
 
 
 def edit(request):
